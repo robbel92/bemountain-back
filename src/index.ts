a65a54f46125334995dae04f01ef5bd2163ec373
Robertt Belenguer Cotaina <rob.belenguer@gmail.com>
2023-05-25 11:23:50 +0200

    Start app listen on port 4000

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,11 @@
-const a = 3;
-export default a;
+import chalk from "chalk";
+import app from "./server/app";
+import createDebug from "debug";
+
+const port = process.env.PORT ?? 4000;
+
+const debug = createDebug("bemount-api:root");
+
+app.listen(port, () => {
+  debug(chalk.blueBright(`Listening in port ${port}`));
+});
