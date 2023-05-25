acaa2ecdb34deb00c033fda39c53368939f91243
Robertt Belenguer Cotaina <rob.belenguer@gmail.com>
2023-05-25 15:48:28 +0200

    Connect app server to datadabase bemount-api

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,15 @@ import "./loadEnvironment.js";
 import chalk from "chalk";
 import app from "./server/app.js";
 import createDebug from "debug";
+import connectToDatabase from "./server/database/connectToDatabase.js";
 
 const port = process.env.PORT ?? 4000;
 
 const debug = createDebug("bemount-api:root");
+
+const mongoDbConnection = process.env.MONGODB_CONNECTION!;
+
+await connectToDatabase(mongoDbConnection);
 
 app.listen(port, () => {
   debug(chalk.blueBright(`Listening in port ${port}`));
