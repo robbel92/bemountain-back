18e575462c922f45ac59951e4f71a41b4556f000
Robertt Belenguer Cotaina <rob.belenguer@gmail.com>
2023-05-25 22:57:06 +0200

    Create loginSchema

---
+++ src/server/schemas/UserSchemas.ts
@@ -0,0 +1,9 @@
+import { Joi } from "express-validation";
+import type { UserLoginStructure } from "../types";
+
+export const loginSchema = {
+  body: Joi.object<UserLoginStructure>({
+    username: Joi.string().required(),
+    password: Joi.string().required(),
+  }),
+};
