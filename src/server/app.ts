4cf5edf93d9923b7d910192536b063300e49a7fb
Robertt Belenguer Cotaina <rob.belenguer@gmail.com>
2023-05-25 11:18:26 +0200

    Configure express.json middleware

--- src/server/app.ts
+++ src/server/app.ts
@@ -4,6 +4,9 @@ import express from "express";
 const app = express();
 
 const allowedOrigins = [process.env.LOCAL_ORIGIN!];
+
 app.use(cors({ origin: allowedOrigins }));
+
+app.use(express.json());
 
 export default app;
